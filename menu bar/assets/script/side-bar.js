7f6ab1055315d9c3efc4bcd7552a72398e286e68
Ernest Malinao <121604020+eagercheese@users.noreply.github.com>
2024-10-14 23:08:24 +0800

    Add files via upload

---
+++ menu bar/assets/script/side-bar.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body"),
+        sideBar = body.querySelector(".sidebar"),
+        barButton = body.querySelector(".menu-bar-button");
+
+barButton.addEventListener("click", () => {
+    sideBar.classList.toggle("close");    
+})
