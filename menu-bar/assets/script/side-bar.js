b292868a6af8fefc390360de298d6bf3de230c75
Ernest Malinao <121604020+eagercheese@users.noreply.github.com>
2024-10-20 22:58:18 +0800

    Delete menu-bar directory

--- menu-bar/assets/script/side-bar.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body"),
-        sideBar = body.querySelector(".sidebar"),
-        barButton = body.querySelector(".menu-bar-button");
-
-barButton.addEventListener("click", () => {
-    sideBar.classList.toggle("close");
-});
-
-
